44f4ecdbe5fc3ab4b19f600d9467a06db8b47635
Jose Alisson <alissonbarbosa.9982@gmail.com>
2024-08-07 17:13:10 -0300

    client

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    API_MAIN : "/main/",
-    API_PEDIDOS : "/pedidos/",
-    API_MAIN_SOCKET: "/socket.io/"
+    API_MAIN : "http://localhost:80/main/",
+    API_PEDIDOS : "http://localhost:80/pedidos/",
+    API_MAIN_SOCKET: "ws://localhost:80"
 }
