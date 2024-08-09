bef48d1f6e86a0ee02f5abd0e69e5cb722c84201
Jose Alisson <alissonbarbosa.9982@gmail.com>
2024-08-08 22:41:57 -0300

    client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject, OnInit, PLATFORM_ID, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EstablishmentService } from './shared/services/establishment/establishment.service';
 import { Title } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +11,27 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
   private establishmentS = inject(EstablishmentService)
   private title = inject(Title)
 
-  constructor(){
+  constructor(
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
     this.establishmentS.getEstablishment().subscribe(data => {
       this.title.setTitle(`${data.ClientNome} | Cliente`)
     })
+  }
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const head = document.head;
+      const link = this.renderer.createElement('link');
+      this.renderer.setAttribute(link, 'rel', 'stylesheet');
+      this.renderer.setAttribute(link, 'href', 'http://localhost/main/style.css');
+      this.renderer.appendChild(head, link);
+    }
   }
 }
